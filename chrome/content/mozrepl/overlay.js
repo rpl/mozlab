e53e251a5f396c8f2c1964ca481cef0c4cf889d2
Luca Greco <luca.greco@alcacoop.it>
2010-05-23 21:17:23 +0200

    FIX: fix fake version in the install.rdf, fixed open tab on firefox to suggest user mozrepl updated xpi

--- chrome/content/mozrepl/overlay.js
+++ chrome/content/mozrepl/overlay.js
@@ -36,23 +36,53 @@
  * ***** END LICENSE BLOCK ***** */
 
 window.addEventListener(
-    'load', function(event) { openMozREPLInstall(); }, false);
+  // TRANSITION MOZLAB -> MOZREPL
+    'load', function(event) { 
+      var Cc = Components.classes;
+      var Ci = Components.interfaces;
 
-function openMozREPLInstall() {
-  var extmgr = Cc["@mozilla.org/extensions/manager;1"].getService(Ci.nsIExtensionManager);
-  var pref = Cc['@mozilla.org/preferences-service;1']
-    .getService(Ci.nsIPrefService)
-    .getBranch('extensions.mozlab.');
+      var appInfo = Cc["@mozilla.org/xre/app-info;1"].
+        getService(Ci.nsIXULAppInfo);
+      var extmgr = Cc["@mozilla.org/extensions/manager;1"].
+        getService(Ci.nsIExtensionManager);
+      var pref = Cc['@mozilla.org/preferences-service;1'].
+        getService(Ci.nsIPrefService).
+        getBranch('extensions.mozlab.');
+
+      var open_url = null;
+
+      function urlconv(spec) {
+        var ios = Cc["@mozilla.org/network/io-service;1"].
+          getService(Ci.nsIIOService);
+        return ios.newURI(spec, null, null);
+      }
+
+
+      // FIREFOX
+      if (appInfo.ID === "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}") {
+        open_url = function(url, name) {
+          return Application.activeWindow.open(urlconv(url));
+        }
+      } // OTHER XULRUNNER APPS
+      else {
+        open_url = function(url, name) {
+          return window.open(url, name, "chrome=yes");
+        }
+      }
+
   
-  if(extmgr.getItemForID("mozrepl@hyperstruct.net") && pref.getBoolPref("firstrun")) {
-    // AUTO-UNINSTALL MOZLAB: extmgr.uninstallItem("mozlab@hyperstruct.net");
-    handle = window.open("chrome://mozlab/content/mozrepl/mozrepl-installed.xul", "mozrepl_install", "chrome=yes,modal=yes");
-    handle.focus();
-    pref.setBoolPref("firstrun", false);
-  }
-  else if (pref.getBoolPref("firstrun")) {
-    handle = window.open("chrome://mozlab/content/mozrepl/mozrepl-install.xul", "mozrepl_install", "chrome=yes,modal=yes");
-    handle.focus();
-  }
-}
+      if(extmgr.getItemForID("mozrepl@hyperstruct.net") && pref.getBoolPref("firstrun")) {
+        // AUTO-UNINSTALL MOZLAB: extmgr.uninstallItem("mozlab@hyperstruct.net");
+        handle = open_url("chrome://mozlab/content/mozrepl/mozrepl-installed.xul", 
+                          "mozrepl_install");
+        handle.focus();
+        pref.setBoolPref("firstrun", false);
+      }
+      else if (pref.getBoolPref("firstrun")) {
+        handle = open_url("chrome://mozlab/content/mozrepl/mozrepl-install.xul", 
+                          "mozrepl_install");
+        handle.focus();
+      }    
+    }, false);
+
 
